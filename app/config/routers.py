6d9886b6220409482de64e777dea0626bcc5dacd
jhon <jhon@m4.local>
2025-11-27 20:54:21 -0500

    Revert "Merge pull request #34 from uts-web-service-grupo5/HU-0017-acceso-catalogo-api"
    This reverts commit e43a1b28e6da2de875fe85a996d03cad67f61dec, reversing
    changes made to d4bf4e8f44a8d0dac174fc225c70dd7e3d4426af.

--- app/config/routers.py
+++ app/config/routers.py
@@ -1,5 +1,4 @@
 from app.api import user_api
 from app.api import ad_usuarios_api
-from app.api import catalogo_api
 
-ROUTERS = [user_api.router, ad_usuarios_api.router, catalogo_api.router]
+ROUTERS = [user_api.router, ad_usuarios_api.router]
--- app/database.py
+++ app/database.py
@@ -21,27 +21,4 @@ class UserDB(Base):
     suscripcion = Column(Integer, nullable=False)
 
 
-class VideojuegoDB(Base):
-    __tablename__ = "videojuegos"
-
-    id_videojuego = Column(Integer, primary_key=True, index=True)
-    nombre_juego = Column(String, nullable=False)
-    restriccion_edad = Column(String, nullable=False)
-    acceso_plan = Column(String, nullable=False)
-
-
-# Tabla de Suscripciones ( temporal, para pruebas en catalogo - Esteban)
-class SuscripcionDB(Base):
-    __tablename__ = "suscripciones"
-
-    id_suscripcion = Column(Integer, primary_key=True, index=True)
-    id_cliente = Column(Integer, nullable=False, index=True)
-    plan = Column(String, nullable=False)  # "Bronce", "Plata", "Oro"
-    estado = Column(String, nullable=False)  # "activo", "inactivo", "vencido"
-    fecha_inicio = Column(String, nullable=False)
-    fecha_vencimiento = Column(String, nullable=True)  # NULL para plan Bronce
-    id_transaccion = Column(Integer, nullable=True)  # NULL para plan Bronce
-    monto_pagado = Column(Integer, nullable=True)
-
-
 Base.metadata.create_all(bind=engine)
